24dffea92917c43c66df529b13361575ea48e336
Azzuhri Nauli Pasaribu <azzuhri.nauli@ui.ac.id>
2022-12-08 09:44:13 +0700

    implemented drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:final_project/page/drawer.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,13 +25,13 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -41,7 +42,7 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
+  final String title = "Home Page";
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -74,7 +75,9 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+      
       ),
+      drawer: myDrawer(context),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
---
+++ lib/page/drawer.dart
@@ -0,0 +1,21 @@
+import 'package:final_project/main.dart';
+import 'package:flutter/material.dart';
+
+Drawer myDrawer(context){
+  return Drawer(
+    child: Column(
+      children: [
+        // Clickable menu
+        ListTile(
+          title: const Text('Home'),
+          onTap: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const MyHomePage())
+            );
+          }
+        ),
+      ],
+    ),
+  );
+}
