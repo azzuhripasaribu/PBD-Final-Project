63136bd650b741b5f9c2daa7d26b5b33e00f949c
Julian Justin Orvino <justinorvino@gmail.com>
2022-12-13 00:19:51 +0700

    Tau ah gelap

--- lib/page/drawer.dart
+++ lib/page/drawer.dart
@@ -1,21 +1,22 @@
-import 'package:final_project/main.dart';
-import 'package:flutter/material.dart';
+//import 'package:final_project/main.dart';
+//import 'package:final_project/page/report_form.dart';
+//import 'package:flutter/material.dart';
 
-Drawer myDrawer(context){
-  return Drawer(
-    child: Column(
-      children: [
+//Drawer myDrawer(context){
+  //return Drawer(
+    //child: Column(
+      //children: [
         // Clickable menu
-        ListTile(
-          title: const Text('Home'),
-          onTap: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => const MyHomePage())
-            );
-          }
-        ),
-      ],
-    ),
-  );
-}
+        //ListTile(
+          //title: const Text('Home'),
+          //onTap: () {
+            //Navigator.pushReplacement(
+              //context,
+              //MaterialPageRoute(builder: (context) => const MyHomePage())
+            //);
+          //}
+        //),
+      //],
+    //),
+  //);
+//}
