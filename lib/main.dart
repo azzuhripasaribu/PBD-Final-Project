e84ff041c9471e51622463f887e05157710fc6a3
Azzuhri Nauli Pasaribu <azzuhri.nauli@ui.ac.id>
2022-12-12 22:09:01 +0700

    implement authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:final_project/page/drawer.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,22 +13,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
+    return Provider(
+        create: (_) {
+          CookieRequest request = CookieRequest();
+          return request;
+        },
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            // This is the theme of your application.
+            //
+            // Try running your application with "flutter run". You'll see the
+            // application has a blue toolbar. Then, without quitting the app, try
+            // changing the primarySwatch below to Colors.green and then invoke
+            // "hot reload" (press "r" in the console where you ran "flutter run",
+            // or simply save your changes to "hot reload" in a Flutter IDE).
+            // Notice that the counter didn't reset back to zero; the application
+            // is not restarted.
+            primarySwatch: Colors.blue,
+          ),
+          home: const MyHomePage(),
+        ));
   }
 }
 
@@ -75,7 +82,6 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
-      
       ),
       drawer: myDrawer(context),
       body: Center(
--- lib/page/drawer.dart
+++ lib/page/drawer.dart
@@ -1,7 +1,11 @@
 import 'package:final_project/main.dart';
 import 'package:flutter/material.dart';
+import 'package:final_project/page/login_page.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
 
-Drawer myDrawer(context){
+Drawer myDrawer(BuildContext context){
+  final request = context.watch<CookieRequest>();
   return Drawer(
     child: Column(
       children: [
@@ -15,6 +19,27 @@ Drawer myDrawer(context){
             );
           }
         ),
+        request.loggedIn? 
+        ListTile(
+          title: Text('logout'),
+          onTap: () {
+            request.loggedIn = false;
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const MyHomePage())
+            );
+          },
+        ):
+        ListTile(
+          title: const Text('login'),
+          onTap: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const LoginPage())
+            );
+          }
+        ),
+        
       ],
     ),
   );
