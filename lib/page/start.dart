63a216f496824cc2f8db3fdc0b1e72781c35a08d
fiqoanugrah <fiqoanugrah@gmail.com>
2022-12-12 18:01:11 +0700

    new things

--- lib/page/start.dart
+++ lib/page/start.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 class StartPage extends StatefulWidget {
   const StartPage({Key? key}) : super(key: key);
@@ -10,6 +11,88 @@ class StartPage extends StatefulWidget {
 class _StartPageState extends State<StartPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 16,
+            ),
+            Container(
+              height: size.height / 1.8,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              child: Image.asset(
+                "assets/start.jpg",
+                // height: 50,
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "We here to help you",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(
+                      right: 100,
+                    ),
+                    child: Text(
+                      "Find your help by reporting your problem",
+                      style: TextStyle(
+                        color: Colors.black38,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  InkWell(
+                    onTap: () => Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return const HomePage();
+                    })),
+                    child: Container(
+                      height: 56,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 226, 77, 124),
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          "Get Started",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
