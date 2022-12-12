e9cb73d72ec949b9a0e2510c33204f43defba578
fiqoanugrah <fiqoanugrah@gmail.com>
2022-12-12 16:47:13 +0700

    commit new directory in main

---
+++ lib/page/start.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StartPage extends StatefulWidget {
+  const StartPage({Key? key}) : super(key: key);
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
